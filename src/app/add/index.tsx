0e1735ca9b2e5c129c94d300efebc8e8c8e13b0e
mtejota <matheustjota23@gmail.com>
2026-04-10 15:05:46 -0300

    feature: create inputs

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,4 +1,5 @@
 import { Categories } from "@/components/categories";
+import { Input } from "@/components/input";
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
@@ -16,6 +17,11 @@ export default function Add() {
       </View>
       <Text style={styles.label}>Selecione uma categoria</Text>
       <Categories />
+
+      <View style={styles.form}>
+        <Input placeholder="Nome" />
+        <Input placeholder="Url" />
+      </View>
     </View>
   );
 }
